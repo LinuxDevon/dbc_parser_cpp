d499bf72468cce4fcd50b186173a09ff69161202
Devon <devonadair96@gmail.com>
2021-02-05 19:48:11 -0500

    Testing a trim function found online

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -74,3 +74,10 @@ end";
 		}
 	}
 }
+
+TEST_CASE("Test the string trim feature", "[string]") {
+	std::string s = "    	there might be some white space....   ";
+
+	REQUIRE(String::trim(s) == "there might be some white space....");
+}
+
