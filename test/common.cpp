386d29beddd23cdf42fea9e7afb63f82a4feaa7d
Devon Adair <devonadair96@gmail.com>
2024-01-28 12:48:16 -0500

    generating unique file names for temp dbc now

--- test/common.cpp
+++ test/common.cpp
@@ -1,8 +1,10 @@
 #include "common.hpp"
 #include "defines.hpp"
+#include <exception>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 
 bool create_tmp_dbc_with(const char* filename, const char* content) {
 	auto* file = std::fopen(filename, "w");
@@ -20,13 +22,19 @@ std::string create_temporary_dbc_with(const char* contents) {
 	std::filesystem::path temp_dir = std::filesystem::temp_directory_path();
 
 	// Generate a unique temporary file name
-	std::filesystem::path temp_file = temp_dir / "temp_file_XXXXXX"; // "XXXXXX" is a placeholder for a unique name
+	char filename[] = "temp_file_XXXXXX";
+	if (std::tmpnam(filename) == nullptr) {
+		throw std::runtime_error("Failed to generate a unique temporary filename.");
+	}
+	std::filesystem::path temp_file = temp_dir / filename;
 
 	std::ofstream file(temp_file);
-	if (file.is_open()) {
-		file << contents << std::endl;
-		file.close();
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to create temporary file.");
 	}
+
+	file << contents << std::endl;
+	file.close();
 
 	return temp_file;
 }
