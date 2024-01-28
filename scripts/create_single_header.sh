f1ec46aa277546841b255a50bef7211949c1a0c3
Devon Adair <devonadair96@gmail.com>
2024-01-28 16:54:53 -0500

    Working script to create a single header file

--- scripts/create_single_header.sh
+++ scripts/create_single_header.sh
@@ -1,1 +1,13 @@
-quom -I include -I src src/dbc.cpp build/libdbc.hpp
+#!/bin/sh
+
+set -e
+
+cargo install cpp-amalgamate
+
+rm -rf build/single_header/
+mkdir -p build/single_header/libdbc
+
+files=$(find src -name "*.cpp")
+files="${files} $(find include -name "*.hpp")"
+
+cpp-amalgamate -d include -d build/_deps/fastfloat-src/include ${files} -o build/single_header/libdbc/libdbc.hpp
